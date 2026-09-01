ba788909992f24aff605726e807bcac3adb2ffcf
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-09-01 14:52:18 -0300

    Estudo de hoje

---
+++ alunos.pl
@@ -0,0 +1,12 @@
+aluno(joao, unb).
+aluno(julia, unb).
+aluno(jonas, unb).
+aluno(lucas, unb).
+
+aluno(davi, fcte).
+aluno(ana, fcte).
+aluno(rodrigo, fcte).
+
+aluno(isaac, ifb).
+aluno(rosa, ifb).
+aluno(maria, ifb).
